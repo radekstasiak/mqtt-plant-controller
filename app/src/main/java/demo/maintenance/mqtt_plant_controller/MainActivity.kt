c96e739f9b893a3ebbad17b28474b0f99b017d45
Radek <radek.stasiak@gmail.com>
2019-03-11 22:32:09 +0000

    Add UI assets and connect to MQTT in onStart()

--- app/src/main/java/demo/maintenance/mqtt_plant_controller/MainActivity.kt
+++ app/src/main/java/demo/maintenance/mqtt_plant_controller/MainActivity.kt
@@ -2,10 +2,12 @@ package demo.maintenance.mqtt_plant_controller
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.Window
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.eclipse.paho.android.service.MqttAndroidClient
@@ -18,38 +20,70 @@ class MainActivity : AppCompatActivity() {
     val MQTT_USER = "vlfnelyd"
     val MQTT_PASS = "nXE1FVBVSJHn"
     val MQTT_PORT = 17236
-    val MQTT_TOPIC_HMDT = "hmdt"
+
     val MQTT_TOPIC_WATER_PUMP = "waterPump"
+    val MQTT_TOPIC_WATER_PUMP_CMD = "waterPump"
+    val MQTT_TOPIC_GET_WATER_PUMP_STATUS = "getWaterPumpStatus"
+    val MQTT_TOPIC_WATER_PUMP_STATUS = "sendWaterPumpStatus"
+    val MQTT_TOPIC_HUMIDITY_LEVEL = "hmdtLevel"
     val MQTT_CMD_START = "start"
     val MQTT_CMD_STOP = "stop"
+    val MQTT_CMD_STATUS = "stop"
+
     var isWaterPumpRunning = false
-    lateinit var hmdtTv:TextView
+    lateinit var hmdtTv: TextView
     val mqttClientId = MqttClient.generateClientId()
     lateinit var mqttClient: MqttAndroidClient
 
+//TODO connect/disconnect on button
+//TODO assign id to the client
+//TODO get connected clients
+//TODO reflect if pump is running or stopped
+//TODO service to receive updates and create notifications
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
         mqttClient = MqttAndroidClient(
             this.applicationContext, "tcp://${MQTT_SERVER_ADDRESS}:${MQTT_PORT}",
             mqttClientId
         )
-        connect()
-        hmdtTv = findViewById<TextView>(R.id.valueTv)
-        fab.setOnClickListener { view ->
-            if(isWaterPumpRunning) {
+        pumpControl.setOnClickListener { view ->
+            if (isWaterPumpRunning) {
                 publish(mqttClient, MQTT_CMD_STOP)
                 isWaterPumpRunning = false
-                Log.d("file","MQTT_CMD_STOP")
-            }else{
+                Log.d("file", "MQTT_CMD_STOP")
+            } else {
                 publish(mqttClient, MQTT_CMD_START)
                 isWaterPumpRunning = true
-                Log.d("file","MQTT_CMD_START")
+                Log.d("file", "MQTT_CMD_START")
             }
 
         }
+    }
+    private fun updatePlantStatus(status:Int){
+        if(status <= 25){
+            plantStatusIv.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.plantstate0))
+        }else if(status > 25 && status <= 50){
+            plantStatusIv.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.plantstate1))
+        }else if (status > 50 && status <=80){
+            plantStatusIv.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.plantstate2))
+        }else{
+            plantStatusIv.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.plantstate3))
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!mqttClient.isConnected){
+            Log.d("file","connecting to mqtt")
+            connect()
+        }
+    }
+
+    private fun getMqttWaterPumpStatus() {
+        if (mqttClient.isConnected) mqttClient.publish(MQTT_TOPIC_GET_WATER_PUMP_STATUS, MqttMessage("".toByteArray()))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -83,7 +117,9 @@ class MainActivity : AppCompatActivity() {
                     // We are connected
                     Log.d("file", "onSuccess")
                     //publish(client,"payloadd");
-                    subscribe(mqttClient, MQTT_TOPIC_HMDT)
+                    subscribe(mqttClient, MQTT_TOPIC_HUMIDITY_LEVEL)
+                    subscribe(mqttClient, MQTT_TOPIC_WATER_PUMP_STATUS)
+                    getMqttWaterPumpStatus()
                     mqttClient.setCallback(object : MqttCallback {
                         override fun connectionLost(cause: Throwable) {
                             Snackbar.make(hmdtTv.rootView, "Connectio Lost", Snackbar.LENGTH_SHORT).show()
@@ -92,8 +128,16 @@ class MainActivity : AppCompatActivity() {
                         @Throws(Exception::class)
                         override fun messageArrived(topic: String, message: MqttMessage) {
                             Log.d("file", message.toString())
-                            if (topic == MQTT_TOPIC_HMDT) {
-                                hmdtTv.text = message.toString()
+                            if (topic == MQTT_TOPIC_HUMIDITY_LEVEL) {
+                                updatePlantStatus(message.toString().toInt())
+                            } else if (topic == MQTT_TOPIC_WATER_PUMP_STATUS) {
+                                if (message.toString().equals("1")) {
+                                    isWaterPumpRunning = true;
+                                } else {
+                                    isWaterPumpRunning = false;
+                                }
+
+                                Log.d("file", "Water pump status: ${message.toString()}")
                             }
 
 
@@ -115,7 +159,7 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
-
+        //TODO update to accept topic as an argument, use throught the activity
     fun publish(client: MqttAndroidClient, payload: String) {
         if (client.isConnected) {
             val topic = MQTT_TOPIC_WATER_PUMP
