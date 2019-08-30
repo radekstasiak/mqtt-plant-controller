76e8a877b53aaceb0d3af7b4e98f67023080e328
Radek <radek.stasiak@gmail.com>
2019-08-30 19:37:30 +0200

    Add heart model to the plane WIP
    - error is "java.util.concurrent.CompletionException: com.google.ar.sceneform.rendering.RendercoreBundle$VersionException: Rendercore bundle (.rcb) version not supported, max version supported is 0.51.X. Version requested for loading is 0.54.2"
    - https://github.com/google-ar/sceneform-android-sdk/issues/312
    - https://proandroiddev.com/building-arcore-apps-using-sceneform-part-3-4cd392d6284f

--- app/src/main/java/demo/maintenance/mqtt_plant_controller/ar/SceneformActivity.kt
+++ app/src/main/java/demo/maintenance/mqtt_plant_controller/ar/SceneformActivity.kt
@@ -1,9 +1,19 @@
 package demo.maintenance.mqtt_plant_controller.ar
 
+import android.graphics.Point
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.google.ar.core.Anchor
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import demo.maintenance.mqtt_plant_controller.R
+import kotlinx.android.synthetic.main.activity_sceneform.*
 
 class SceneformActivity : AppCompatActivity() {
 
@@ -13,5 +23,54 @@ class SceneformActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sceneform)
 
         fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
+        fab.setOnClickListener {
+            addObject(Uri.parse("heart.sfb"))
+        }
+    }
+
+    private fun addObject(parse: Uri) {
+        val frame = fragment.arSceneView.arFrame
+        val point = getScreenCenter()
+        if (frame != null) {
+            val hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    placeObject(fragment, hit.createAnchor(), parse)
+                    break
+                }
+            }
+        }
+    }
+
+    private fun placeObject(fragment: ArFragment, createAnchor: Anchor, model: Uri) {
+        ModelRenderable.builder()
+            .setSource(fragment.context, model)
+            .build()
+            .thenAccept {
+                addNodeToScene(fragment, createAnchor, it)
+            }
+            .exceptionally {
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage(it.message)
+                    .setTitle("error!")
+                val dialog = builder.create()
+                dialog.show()
+                return@exceptionally null
+            }
+    }
+
+    private fun addNodeToScene(fragment: ArFragment, createAnchor: Anchor, renderable: ModelRenderable) {
+        val anchorNode = AnchorNode(createAnchor)
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderable
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+
+    private fun getScreenCenter(): android.graphics.Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return Point(vw.width / 2, vw.height / 2)
     }
 }
